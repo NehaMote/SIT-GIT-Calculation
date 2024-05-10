2005239fb79c6b957b808bbdc6edf3d098a528ba
NehaMote <nehamote18@gmail.com>
2024-05-10 10:57:37 +0530

    changes in Addition.java file by Neha

--- src/com/sit/git/calculation/Addition.java
+++ src/com/sit/git/calculation/Addition.java
@@ -2,7 +2,10 @@ package com.sit.git.calculation;
 
 public class Addition {
    public static void add() {
-	   
+	 int a=10;
+	 int b=20;
+	 int c=a+b;
+	 System.out.println("Addition of a and b :"+c);
    }
 	public static void main(String[] args) {
 		add();
