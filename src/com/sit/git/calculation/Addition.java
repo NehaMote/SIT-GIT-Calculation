c7cd83c93b7d2a060c9fe4e0142874ca1e3d54e5
NehaMote <nehamote18@gmail.com>
2024-05-10 10:45:25 +0530

    Calculation project

---
+++ src/com/sit/git/calculation/Addition.java
@@ -0,0 +1,12 @@
+package com.sit.git.calculation;
+
+public class Addition {
+   public static void add() {
+	   
+   }
+	public static void main(String[] args) {
+		add();
+
+	}
+
+}
---
+++ src/com/sit/git/calculation/Test.java
@@ -0,0 +1,9 @@
+package com.sit.git.calculation;
+
+public class Test {
+	public static void main(String[] args) {
+		Addition.add();
+	}
+	
+
+}
