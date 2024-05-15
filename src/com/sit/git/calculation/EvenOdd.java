f388c7cffa689f26632e89e09388e7f4fa66eed4
prajaktajag <jagtaprajakta213@gmail.com>
2024-05-14 21:52:52 -0700

    Added Even & Odd number program

--- src/com/sit/git/calculation/EvenOdd.java
+++ src/com/sit/git/calculation/EvenOdd.java
@@ -3,11 +3,11 @@ package com.sit.git.calculation;
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		int a=8;
+		int a=5;
 		if(a%2==0) {
 			System.out.println("Given number is even ");
 		}else {
-			System.out.println("Given number is odd number");
+			System.out.println("Given number is odd ");
 		}
 			
 
